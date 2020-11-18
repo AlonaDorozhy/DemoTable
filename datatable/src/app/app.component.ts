a62ce05acae8f1d57c29f18fc601978a619271de
Alona Dorozhynska <adoroz@softserveinc.com>
2020-11-18 16:42:50 +0200

    basic table

--- datatable/src/app/app.component.ts
+++ datatable/src/app/app.component.ts
@@ -1,23 +1,44 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-//   template: `
-//   <!-- <div>
-//     <ngx-datatable [rows]="rows" [columns]="columns"> </ngx-datatable>
-//   </div> -->
-// `,
   styleUrls: ['./app.component.sass']
 })
-// export class AppComponent {
-//   title = 'datatable';
-// }
+
 export class AppComponent {
-  rows = [
-    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
-    { name: 'Dany', gender: 'Male', company: 'KFC' },
-    { name: 'Molly', gender: 'Female', company: 'Burger King' }
+  employees = require('../app/employees.json');
+  rows = this.employees.map((row: { [x: string]: any; }) => ({
+    name: row['firstName'],
+    lastName: row['lastName'],
+    jobTitleName: row['jobTitleName'],
+    emailAddress: row['emailAddress'],
+    phoneNumber: row['phoneNumber']
+
+
+  }))
+
+  columns = [
+    {
+      prop: 'name',
+      name: 'First Name'
+    },
+    {
+      prop: 'lastName',
+      name: 'Last Name'
+    },
+    {
+      prop: 'jobTitleName',
+      name: 'Job Title'
+    },
+    {
+      prop: 'emailAddress',
+      name: 'Email'
+    },
+    {
+      prop: 'phoneNumber',
+      name: 'Phone'
+    },
+
   ];
-  columns = [{ prop: 'name' }, { name: 'Gender' }, { name: 'Company' }];
+  constructor() {}
 }
