48779a05436eede0ade155d0b36d41c9de6d4b36
Alona Dorozhynska <adoroz@softserveinc.com>
2020-11-18 17:42:05 +0200

    Error on  require

--- datatable/src/app/app.component.ts
+++ datatable/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import example from '../app/employees.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,7 +7,10 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  employees = require('../app/employees.json');
+ private employees = require('./employees.json');
+  constructor() {
+    console.log(this.employees);
+  }
   rows = this.employees.map((row: { [x: string]: any; }) => ({
     name: row['firstName'],
     lastName: row['lastName'],
@@ -40,5 +44,5 @@ export class AppComponent {
     },
 
   ];
-  constructor() {}
+ 
 }
